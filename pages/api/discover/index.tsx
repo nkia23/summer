93577c6957d3497a1e1ae9edb53a87b062047c70
Andrei David <andrei.david@gmail.com>
2023-01-30 21:25:00 +0000

    Small change to test build time.

--- pages/api/discover/index.tsx
+++ pages/api/discover/index.tsx
@@ -2,12 +2,12 @@ import { withSentry } from '@sentry/nextjs'
 import { get } from 'handlers/discover/get'
 import { NextApiHandler } from 'next'
 
-const handler: NextApiHandler = async (req, res2) => {
+const handler: NextApiHandler = async (req, res3) => {
   switch (req.method) {
     case 'GET':
-      return await get(req, res2)
+      return await get(req, res3)
     default:
-      return res2.status(405).end()
+      return res3.status(405).end()
   }
 }
 export default withSentry(handler)
