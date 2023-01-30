d6f805e478b349f79ebfe691ba37e851e4bdd4c6
Andrei David <andrei.david@gmail.com>
2023-01-30 16:11:42 +0000

    Small test change.

--- pages/api/discover/index.tsx
+++ pages/api/discover/index.tsx
@@ -2,12 +2,12 @@ import { withSentry } from '@sentry/nextjs'
 import { get } from 'handlers/discover/get'
 import { NextApiHandler } from 'next'
 
-const handler: NextApiHandler = async (req, res) => {
+const handler: NextApiHandler = async (req, res2) => {
   switch (req.method) {
     case 'GET':
-      return await get(req, res)
+      return await get(req, res2)
     default:
-      return res.status(405).end()
+      return res2.status(405).end()
   }
 }
 export default withSentry(handler)
